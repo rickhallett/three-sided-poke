12d0d6685982247ca4b7faf3c5dc92807dd9818b
Rick Hallett <rickhallett@live.co.uk>
2021-05-20 14:32:23 +0100

    map pokemon api response to page test

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
 import Head from "next/head";
+import axios from "axios";
 
-export default function Home() {
-  return <div>Hello, world!</div>;
-}
+const Index = ({ pokemon }) => {
+  return (
+    <div>
+      Hello, Pokedex!
+      {pokemon.map((pokemon, i) => {
+        return (
+          <div>
+            {i + 1}:{pokemon.name}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+Index.getInitialProps = async () => {
+  const pokemon = await axios
+    .get("https://pokeapi.co/api/v2/pokemon?limit=1000")
+    .then((response) => response.data.results);
+  return { pokemon };
+};
+
+export default Index;
