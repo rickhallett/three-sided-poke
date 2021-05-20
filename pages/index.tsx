8fd9f68c9e032d770997a306023a5db4ad9b508b
Rick Hallett <rickhallett@live.co.uk>
2021-05-20 14:44:06 +0100

    add typescript, convert to typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import axios from "axios";
 
+type Pokemon = {
+  name: string;
+};
+
 const Index = ({ pokemon }) => {
   return (
     <div>
       Hello, Pokedex!
-      {pokemon.map((pokemon, i) => {
+      {pokemon.map((pokemon: Pokemon, i: number) => {
         return (
-          <div>
+          <div key={i}>
             {i + 1}:{pokemon.name}
           </div>
         );
