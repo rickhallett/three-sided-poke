c5b0e88ed78af066f4668f4f4fd5296f356ad0a6
Rick Hallett <rickhallett@live.co.uk>
2021-05-24 10:58:06 +0100

    improve detail.jsx styling

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -11,50 +11,51 @@ const Detail = ({ pokemon }: { pokemon: Pokemon }): JSX.Element => {
 
   return (
     <React.Fragment>
-      <div className="flex-col text-center p-1 m-1 border-solid border-4 border-gray-50 rounded-2xl shadow-lg hover:border-gray-100 hover:bg-gray-100">
+      <div className="w-1/2 mx-auto sm:w-full flex-col text-center p-1 m-1 border-solid border-4 border-gray-50 rounded-2xl shadow-lg hover:border-gray-100 hover:bg-gray-100">
         <div className="flex w-full justify-between p-3">
-          <div className="text-3xl text-gray-600 tracking-wider">
+          <div className="text-base sm:text-3xl text-gray-600 tracking-wider">
             {pokemon.name}
           </div>
           <div className="text-3xl font-bold text-blue-300">#{pokemon.id}</div>
         </div>
 
-        <div className="flex flex-row-reverse justify-evenly">
+        <div className="flex flex-col sm:flex-row-reverse justify-center">
           <div>
             {/* <img src={pokemon.sprites.front_default}></img> */}
             <img
               src={`/images/sprites/${pokemon.id}.png`}
               width={300}
-              className="mx-auto"
+              className="mx-auto hidden sm:block"
+            ></img>
+            <img
+              src={`/images/sprites/${pokemon.id}.png`}
+              width={150}
+              className="mx-auto sm:hidden"
             ></img>
           </div>
-          <div className="text-center my-auto">
+          <div className="text-center my-auto text-sm md:text-lg">
             <ul>
               <li>Height: {pokemon.height}</li>
               <li>Height: {pokemon.weight}</li>
               {pokemon.stats.map((stat, i) => (
                 <li key={i} className={i == 1 || i == 3 ? "mt-3" : null}>
-                  {i}
                   {stat.stat.name}: {stat.base_stat}
                 </li>
               ))}
             </ul>
           </div>
         </div>
-        <div className="flex justify-center mt-2">
-          <div className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">
+        <div className="flex justify-center my-5 sm:mt-2">
+          <div className="bg-gray-300 hover:bg-gray-400 text-white text-xs sm:text-base font-bold py-3 sm:py-2 px-2 sm:px-4 rounded ">
             <Link href={pokemon.id === 1 ? "/" : `/pokemon/${pokemon.id - 1}`}>
               Previous
             </Link>
           </div>
 
-          <div className="bg-gray-300 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded ml-5">
+          <div className="bg-gray-300 hover:bg-gray-400 text-white text-xs sm:text-base font-bold py-3 sm:py-2 px-2 sm:px-4 ml-5 rounded">
             <Link href={`/pokemon/${pokemon.id + 1}`}>Next</Link>
           </div>
         </div>
-      </div>
-      <div>
-        <Link href="/">Home</Link>
       </div>
     </React.Fragment>
   );
