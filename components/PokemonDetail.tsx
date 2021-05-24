64388684e43c693d262446c0496375c1784608c9
Rick Hallett <rickhallett@live.co.uk>
2021-05-24 22:30:05 +0100

    basic implementation of battleArena page

---
+++ components/PokemonDetail.tsx
@@ -0,0 +1,45 @@
+const PokemonDetail = ({ pokemon }) => {
+  if (!pokemon) {
+    return null;
+  }
+  console.log(pokemon);
+  return (
+    <div className="bg-white w-1/2 mx-auto sm:w-full flex-col text-center p-1 m-1 border-solid border-4 border-gray-50 rounded-2xl shadow-lg">
+      <div className="flex w-full justify-between p-3">
+        <div className="text-base sm:text-3xl text-gray-600 tracking-wider">
+          {pokemon.name}
+        </div>
+        <div className="text-3xl font-bold text-blue-300">#{pokemon.id}</div>
+      </div>
+
+      <div className="flex flex-col sm:flex-row-reverse justify-center">
+        <div>
+          <img
+            src={`/images/sprites/${pokemon.id}.png`}
+            width={300}
+            className="mx-auto hidden sm:block"
+          ></img>
+          <img
+            src={`/images/sprites/${pokemon.id}.png`}
+            width={150}
+            className="mx-auto sm:hidden"
+          ></img>
+        </div>
+        <div className="text-center my-auto text-sm md:text-lg">
+          <ul>
+            <li>Height: {pokemon.height}</li>
+            <li>Height: {pokemon.weight}</li>
+            {pokemon.stats &&
+              pokemon.stats.map((stat, i) => (
+                <li key={i} className={i == 1 || i == 3 ? "mt-3" : null}>
+                  {stat.stat.name}: {stat.base_stat}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonDetail;
---
+++ pages/battleArena.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import { Pokemon } from "../inferfaces/pokemon.types";
+import PokemonDetail from "../components/PokemonDetail";
+
+const BattleArena = () => {
+  const [favourites, setFavourites] = useState<Partial<Pokemon>[]>();
+  const [contenderOne, setContenderOne] = useState<Partial<Pokemon>>();
+  const [contenderTwo, setContenderTwo] = useState<Partial<Pokemon>>();
+
+  useEffect(() => {
+    const fetchLocalStorage = async () => {
+      const browserFavouritesStore = window.localStorage.getItem(
+        "three-sided-pokedex:favourites"
+      );
+
+      const storeObj = JSON.parse(browserFavouritesStore);
+      if (!storeObj) {
+        return;
+      }
+
+      console.log(storeObj);
+
+      setFavourites(storeObj.favourites);
+    };
+
+    fetchLocalStorage();
+  }, []);
+
+  const handleContenderOneSelect = (event) => {
+    console.log(event.target.value);
+    console.log(favourites);
+    setContenderOne(favourites.find((p) => p.name === event.target.value));
+  };
+
+  const handleContenderTwoSelect = (event) => {
+    console.log(event.target.value);
+    console.log(favourites);
+    setContenderTwo(favourites.find((p) => p.name === event.target.value));
+  };
+
+  return (
+    <div>
+      <div className="flex justify-around mx-auto text-gray-600 m-5">
+        <div className="flex my-auto">
+          <label className="self-center">Contender 1</label>
+          <select
+            className="border-2 border-gray-300 rounded-full text-gray-400 h-10 w-60 pl-5 pr-10 ml-5 bg-white text-sm hover:border-gray-400 focus:outline-none appearance-none"
+            onInput={(event) => handleContenderOneSelect(event)}
+          >
+            {favourites &&
+              favourites.map((f, i) => (
+                <option key={f.name + i}>{f.name}</option>
+              ))}
+          </select>
+        </div>
+
+        <div className="flex my-auto">
+          <label className="self-center">Contender 2</label>
+          <select
+            className="border-2 border-gray-300 rounded-full text-gray-400 h-10 w-60 pl-5 pr-10 ml-5 bg-white text-sm hover:border-gray-400 focus:outline-none appearance-none"
+            onInput={(event) => handleContenderTwoSelect(event)}
+          >
+            {favourites &&
+              favourites.map((f, i) => (
+                <option key={f.name + i}>{f.name}</option>
+              ))}
+          </select>
+        </div>
+      </div>
+      <PokemonDetail pokemon={contenderOne} />
+      <PokemonDetail pokemon={contenderTwo} />
+    </div>
+  );
+};
+
+export default BattleArena;
