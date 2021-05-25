828ba7a2d3da09f3357670e1234f2dd812f07963
Rick Hallett <rickhallett@live.co.uk>
2021-05-25 08:20:26 +0100

    IndexCardSave, LoadingSaveButton. StarSaveButton, file for props interfaces

--- components/IndexCard.tsx
+++ components/IndexCard.tsx
@@ -5,6 +5,7 @@ import IndexCardHeader from "../components/IndexCardHeader";
 import { printLocalStorage } from "../util/debug";
 import { LOCAL_URI } from "../config/config";
 import { IndexCardSprite } from "./IndexCardSprite";
+import { IndexCardSave } from "./IndexCardSave";
 
 let fetchLocalStorage;
 
@@ -155,37 +156,11 @@ const IndexCard = (props) => {
     >
       <IndexCardHeader pokemon={pokemon} />
       <IndexCardSprite pokemon={pokemon} />
-      {pokemon && pokemon.stats ? (
-        <svg
-          className="h-6 w-6 m-2"
-          fill={isFavourite[pokemon ? pokemon.id : null] ? "yellow" : "white"}
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          style={{ zIndex: 1000 }}
-          onClick={(event) => handlePokemonSave(event, pokemon)}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-          />
-        </svg>
-      ) : (
-        <svg
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-          />
-        </svg>
-      )}
+      <IndexCardSave
+        pokemon={pokemon}
+        isFavourite={isFavourite}
+        handlePokemonSave={handlePokemonSave}
+      />
     </div>
   );
 };
---
+++ components/IndexCardSave.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { LoadingSaveButton } from "./LoadingSaveButton";
+import { StarSaveButton } from "./StarSaveButton";
+
+export const IndexCardSave = ({
+  pokemon,
+  isFavourite,
+  handlePokemonSave,
+}) => {
+  return (
+    <React.Fragment>
+      {pokemon && pokemon.stats ? (
+        <StarSaveButton
+          pokemon={pokemon}
+          isFavourite={isFavourite}
+          handlePokemonSave={handlePokemonSave}
+        />
+      ) : (
+        <LoadingSaveButton />
+      )}
+    </React.Fragment>
+  );
+};
