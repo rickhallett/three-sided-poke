19f2e59ec5a809ea80bc503ad585cbab2fe6d4d5
Rick Hallett <rickhallett@live.co.uk>
2021-05-25 12:24:09 +0100

    switch test code to new branch

--- components/PokemonDetail.tsx
+++ components/PokemonDetail.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Link from "next/link";
 
-const PokemonDetail = ({ pokemon, children }) => {
+const PokemonDetail = ({ pokemon }) => {
   if (!pokemon) {
     return null;
   }
   console.log(pokemon);
   return (
     <div className="bg-white w-1/2 mx-auto sm:w-full flex-col text-center p-1 m-1 border-solid border-4 border-gray-50 rounded-2xl shadow-lg">
-
       <div className="flex w-full justify-between p-3">
         <div className="text-base sm:text-3xl text-gray-600 tracking-wider">
           {pokemon.name}
@@ -17,7 +16,6 @@ const PokemonDetail = ({ pokemon, children }) => {
       </div>
 
       <div className="flex flex-col sm:flex-row-reverse justify-center">
-
         <div>
           <img
             src={`/images/sprites/${pokemon.id}.png`}
@@ -30,7 +28,7 @@ const PokemonDetail = ({ pokemon, children }) => {
             className="mx-auto sm:hidden"
           ></img>
         </div>
-        
+
         <div className="text-center my-auto text-sm md:text-lg">
           <ul>
             <li>Height: {pokemon.height}</li>
@@ -43,8 +41,6 @@ const PokemonDetail = ({ pokemon, children }) => {
               ))}
           </ul>
         </div>
-
-        {children && <Component>{children}</Component>}
       </div>
     </div>
   );
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -18,24 +18,14 @@ const Detail = ({ pokemon }: { pokemon: Pokemon }): JSX.Element => {
   return (
     <React.Fragment>
       <div className="bg-white w-1/2 mx-auto sm:w-full flex-col text-center p-1 m-1 border-solid border-4 border-gray-50 rounded-2xl shadow-lg">
-        {/* <div className="flex w-full justify-between p-3">
-          <div className="text-base sm:text-3xl text-gray-600 tracking-wider">
-            {pokemon.name}
-          </div>
-          <div className="text-3xl font-bold text-blue-300">#{pokemon.id}</div>
-        </div> */}
-
         <IndexCardHeader pokemon={pokemon} />
-
         <PokemonDetailBody pokemon={pokemon} />
-
         <div className="flex justify-center my-5 sm:mt-2">
           <div className="bg-gray-300 hover:bg-gray-400 text-white text-xs sm:text-base font-bold py-3 sm:py-2 px-2 sm:px-4 rounded ">
             <Link href={pokemon.id === 1 ? "/" : `/pokemon/${pokemon.id - 1}`}>
               Previous
             </Link>
           </div>
-
           <div className="bg-gray-300 hover:bg-gray-400 text-white text-xs sm:text-base font-bold py-3 sm:py-2 px-2 sm:px-4 ml-5 rounded">
             <Link href={`/pokemon/${pokemon.id + 1}`}>Next</Link>
           </div>
